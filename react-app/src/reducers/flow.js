8d397af15cbf017ff250d048ecef7650a4406cc1
Tanyoung Kim <tanyoungk@yahoo-inc.com>
2017-06-11 14:50:52 -0700

    Finalize old Angular1 app branch

--- react-app/src/reducers/flow.js
+++
@@ -1,9 +0,0 @@
-//props triggered by user interaction
-
-export const selectedFriend = (state = {}, action) => {
-  if (action.type === 'SET_FLOW_FRIEND') {
-    return Object.assign({}, state, action.data)
-  } else {
-    return state;
-  }
-}
